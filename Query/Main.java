b91915a0b44de25425938aed39df54989d816b90
Frank-Zhang <zhangxin.wind23@gmail.com>
2017-08-16 11:49:23 +0900

    Commit test

--- Query/Main.java
+++ Query/Main.java
@@ -56,6 +56,7 @@ public class Main {
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
+    // commit test
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
